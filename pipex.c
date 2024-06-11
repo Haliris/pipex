a2a89d41131dc4901d331a10600bb05603f471cc
Haliris <jeanteissier78@gmail.com>
2024-06-11 19:44:53 +0200

    fixed shitty fucking gitignore messing up my project

---
+++ pipex.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jteissie <jteissie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 14:19:59 by jteissie          #+#    #+#             */
+/*   Updated: 2024/06/11 19:40:47 by jteissie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "pipex.h"
+
+void	try_direct_path(char **command, char **env)
+{
+	char	*exec_path;
+
+	if (access(command[0], F_OK | X_OK) == 0)
+	{
+		exec_path = command[0];
+		if (execve(exec_path, command, env) == -1)
+		{
+			trash(command);
+			handle_error(strerror(errno), errno);
+		}
+	}
+}
+
+void	execute(char *av, char **env)
+{
+	char	**command;
+	char	*exec_path;
+
+	command = ft_split(av, ' ');
+	if (!command || !command[0])
+		handle_error("Command split error.\n", EXIT_FAILURE);
+	try_direct_path(command, env);
+	exec_path = get_execpath(env, command[0]);
+	if (!exec_path)
+	{
+		trash(command);
+		handle_error(strerror(PATH_ERROR), PATH_ERROR);
+	}
+	if (execve(exec_path, command, env) == -1)
+	{
+		free(exec_path);
+		trash(command);
+		handle_error(strerror(errno), errno);
+	}
+}
+
+void	first_process(char **av, char **env, int *p_fd)
+{
+	int	fd;
+
+	fd = open(av[1], O_RDONLY, 0777);
+	if (fd == -1)
+		handle_error(strerror(errno), errno);
+	close(p_fd[0]);
+	dup2(fd, STDIN_FILENO);
+	dup2(p_fd[1], STDOUT_FILENO);
+	execute(av[2], env);
+}
+
+void	second_process(char **av, char **env, int *p_fd)
+{
+	int	fd;
+
+	fd = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd == -1)
+		handle_error(strerror(errno), errno);
+	close(p_fd[1]);
+	dup2(fd, STDOUT_FILENO);
+	dup2(p_fd[0], STDIN_FILENO);
+	execute(av[3], env);
+}
+
+int	main(int ac, char *av[], char *envp[])
+{
+	int		fd[2];
+	pid_t	pid;
+	int		pid_status;
+
+	if (ac < 5)
+		handle_error("Error: Expected file1 cmd1 cmd2 file2\n", EXIT_FAILURE);
+	if (pipe(fd) == -1)
+		exit(EXIT_FAILURE);
+	pid = fork();
+	if (pid < 0)
+		exit(EXIT_FAILURE);
+	if (pid == 0)
+		first_process(av, envp, fd);
+	waitpid(pid, &pid_status, 0);
+	if (pid_status)
+		handle_error("Child exited early.", EXIT_FAILURE);
+	second_process(av, envp, fd);
+}
